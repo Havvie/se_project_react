7cbe7996c54700b5c4c25c8a84cb7f12f5878b63
Havvie <164694061+Havvie@users.noreply.github.com>
2026-01-24 01:08:24 -0500

    Final submission ready

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css"
+import logo from "../../assets/wtwr.svg";
+import avatar from "../../assets/avatar.svg";
+
+function Header({ handleAddClick, weatherData }) {
+     const currentDate = new Date().toLocaleString('default', { 
+        month: 'long', day: 'numeric' });
+    return (
+        <header className="header">
+            <img src={logo} alt="WTWR Logo" className="header__logo" />
+            <p className="header__date-and-location">{currentDate}, {weatherData.city}</p>
+            <button type="button" className="header__add-clothes-btn" onClick={handleAddClick}>+ Add Clothes</button>
+            <div className="header__user-container">
+                <p className="header__username">Terrence Tegegne</p>
+                <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+            </div>
+        </header>
+    );
+
+    const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
+
+    function toggleMobileMenu() {
+        setIsMobileMenuOpened((prev) => !prev);
+    }
+}
+
+export default Header;
---
+++ src/utils/weatherApi.js
@@ -0,0 +1,38 @@
+export const getWeather =({ latitude, longitude, APIkey }) => {
+    return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    ).then((res) => {
+        if (res.ok) {
+            return res.json()
+        } else {
+            return Promise.reject(`Error: ${res.status}`)
+        }
+    });
+};
+
+export const filterWeatherData = (data) => {
+    const result = {};
+    result.city = data.name;
+    result.temp = { F: data.main.temp };
+    result.type = getWeatherType(result.temp.F);
+    result.condition = data.weather[0].main.toLowerCase();
+    result.isDayTime = isDaytime({ sunrise: data.sys.sunrise, sunset: data.sys.sunset });
+    return result;
+};
+
+const isDaytime = ({ sunrise, sunset }) => {
+    const now = Date.now();
+    return sunrise * 1000 < now && now < sunset * 1000;
+    // const currentHour = now.getHours();
+    // return currentHour >= (new Date(sunrise * 1000).getHours()) && currentHour < (new Date(sunset * 1000).getHours());
+};
+
+const getWeatherType =(weather) => {
+if (weather >= 86) {
+   return 'hot';
+ } else if (weather >= 66 && weather < 86) {
+   return 'warm';
+ } else {
+   return 'cold';
+ }
+}
