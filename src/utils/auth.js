3bb5e34f3a10b6c5f8d6213f5a0a97d6e709f302
Havvie <164694061+Havvie@users.noreply.github.com>
2026-04-03 23:35:57 -0400

    Sprint 15 initial

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,6 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = process.env.NODE_ENV === "production" 
+  ? "https://api.wtwr.abrasantia.com.ar" 
+  : "http://localhost:3001";
 
 const handleResponse = (res) => {
   if (!res.ok) {
